b725631d3b4119a6c29911b64a25feb939dca6e8
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-15 14:50:30 +0100

    commented out format

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,19 +4,23 @@ from src.models import ItemType
 
 
 def lambda_handler(event, context):
-    fmt = {
-        "id": "123",
-        "refresh_token": "",
-        "spotify_data": [
-            {
-                "top_items": [
-                    {"id": "123", "position": 1}
-                ],
-                "item_type": "artists",
-                "time_range": "long_term"
-            }
-        ]
-    }
+    # fmt = {
+    #     "id": "123",
+    #     "refresh_token": "",
+    #     "spotify_data": [
+    #         {
+    #             "top_items": [
+    #                 {"id": "123", "position": 1}
+    #             ],
+    #             "item_type": "artists",
+    #             "time_range": "long_term"
+    #         }
+    #     ]
+    # }
+
+    # 1. Extract user_id, refresh_token and spotify_data from event records
+    # 2. Update user's spotify refresh_token in DB
+    # 3. Add user's spotify top items to DB
 
     record = event["Records"][0]
     data = json.loads(record["body"])
